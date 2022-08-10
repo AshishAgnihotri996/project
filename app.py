bb1b21c9e8d727c4ca3fe459068a0ea3320fa86c
AshishAgnihotri996 <ashishagnihotri996@gmail.com>
2022-08-10 17:02:56 +0530

    pg conect6

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy 
 from flask_migrate import Migrate, MigrateCommand
-from flask_script import Manager 
 from flask_uploads import UploadSet, configure_uploads, IMAGES
 from flask_login import LoginManager, UserMixin, login_user, login_required, current_user, logout_user
 
@@ -43,8 +42,8 @@ def time_since(delta):
 
 from views import *
 
-manager = Manager(app)
-manager.add_command('db', MigrateCommand)
+
+
 
 if __name__ == '__main__':
-    manager.run()
+    app.run()
