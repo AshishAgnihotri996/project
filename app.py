4cd3ba1f50fbc123b876917433455364a588f010
AshishAgnihotri996 <ashishagnihotri996@gmail.com>
2022-08-18 15:28:54 +0530

    removeing manager

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy 
 from flask_migrate import Migrate, MigrateCommand #to migrate the data with command db
-#from flask_script import Manager
+from flask_script import Manager
 from flask_uploads import UploadSet, configure_uploads, IMAGES #uploads not working instead , reuploaded is working
 from flask_login import LoginManager
 
@@ -10,10 +10,10 @@ app = Flask(__name__)
 #instantiate
 photos = UploadSet('photos', IMAGES)
 
-app.config['UPLOADED_PHOTOS_DEST'] = 'tmp'
+app.config['UPLOADED_PHOTOS_DEST'] = 'images'
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/User/Desktop/twitter-clone/engage.db'
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:''@localhost/new_clone'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://fzlhtaiwunmeak:e1a639d47e78bd9ea37de5d4f63f7560fe1427312e0214ae90aad701ecb0d1dd@ec2-3-223-242-224.compute-1.amazonaws.com:5432/d24g04jsjb4men'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://uhmmfxnusjcuzn:c2fe993a81dd023ab9017003349aff9e1c110f65239749cb8a2b3b16907eb058@ec2-44-205-112-253.compute-1.amazonaws.com:5432/dbpkmc0aiuhrba'
 app.config['DEBUG'] = True
 app.config['SECRET_KEY'] = 'ksdlfkdsofidsithnaljnfadksjhfdskjfbnjewrhewuirhfsenfdsjkfhdksjhfdslfjasldkj' #cross seite request handling
 
@@ -49,8 +49,8 @@ def time_since(delta):
 from views import *
 
 #command attached with db
-# manager = Manager(app)
-# manager.add_command('db', MigrateCommand)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
-    app.run()
+    manager.run()
